025fffd836e56cc13e4d95a05063aa566fa59f0c
paramighty <avinnosatta@gmail.com>
2023-01-03 16:46:02 +0100

    Incorporated Navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import './App.css';
-import "./styles/fonts.css"
-import Header from './components/Sections/Intro/header';
-import Products from './components/Sections/Products/products';
-import Features from './components/Sections/Features/features';
-import Populars from './components/Sections/Populars/populars';
-import Promos from './components/Sections/Promos/promos';
-import Footer from './components/Sections/Footer/footer';
-import Intro from './components/Sections/Intro/intro';
+import "./App.css";
+import "./styles/fonts.css";
+import Header from "./components/Sections/Intro/header";
+import Products from "./components/Sections/Products/products";
+import Features from "./components/Sections/Features/features";
+import Populars from "./components/Sections/Populars/populars";
+import Promos from "./components/Sections/Promos/promos";
+import Footer from "./components/Sections/Footer/footer";
+import Intro from "./components/Sections/Intro/intro";
+import Navbar from "./components/Sections/Navbar/Navbar";
 function App() {
   return (
     <div className="App">
-      <div className='Header-Intro-Wrapper'>
-        <Header />
+      <div className="Header-Intro-Wrapper">
+        <Navbar />
         <Intro />
       </div>
+
       <Products />
       <Features />
       <Populars />
