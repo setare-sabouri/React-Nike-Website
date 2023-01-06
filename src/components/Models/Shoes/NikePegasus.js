46e3f611ef894a8b871c7f7173d26f7722653671
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2023-01-07 00:22:30 +0100

    8Merge branch 'master' of https://github.com/setare-sabouri/React-Nike-Website

--- src/components/Models/Shoes/NikePegasus.js
+++ src/components/Models/Shoes/NikePegasus.js
@@ -1,14 +1,22 @@
-import { useState } from "react";
+
+import { useState, useRef, useEffect } from "react";
 const NikePegasus = ({ materials, nodes }) => {
-    const [Hover, setHover] = useState(false);
+    const [ScWidth, setWidth] = useState(window.innerWidth);
+    const meshEl = useRef();
+    meshEl.current = ScWidth;
+    useEffect(() => {
+        window.addEventListener('resize', function () {
+            setWidth(window.innerWidth);
+        })
+    })
     return (
         <mesh
+            ref={meshEl}
             geometry={nodes.defaultMaterial.geometry}
             material={materials.NikeShoe}
-            position={[1, 0, 0]}
-            onPointerOver={() => setHover(!Hover)}
-            onPointerOut={() => setHover(false)}
-            scale={Hover ? 1.5 : 1}
+            position={[0, meshEl.current < 800 ? -0.3 : -0.3, 0]}
+            scale={(meshEl.current < 768) ? 0.6 : ((meshEl.current > 768 && meshEl.current < 1024) ? 0.7 : (1024 < meshEl.current && meshEl.current < 1200) ? 1 : 1.3) //ask for refactoring this line ! 
+            }
         // map={materials.NikeShoe.normalMap}
         />
     )
