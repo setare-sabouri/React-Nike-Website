b284c47d6ca4fc31ae2dd15ccc7944a8985eeb6c
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2023-01-06 19:38:11 +0100

    updates on carousel

--- src/components/Sections/Populars/Carousel.js
+++ src/components/Sections/Populars/Carousel.js
@@ -15,7 +15,7 @@ function PopularsCarousel() {
                 activeSlideIndex={activeSlideIndex}
                 onRequestChange={setActiveSlideIndex}
                 itemsToShow={3}
-                itemsToScroll={3}
+                itemsToScroll={1}
                 showSlidesBeforeInit={true}
                 forwardBtnProps={{
                     className: "Carousel-btn right",
