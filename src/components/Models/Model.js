19d9c21126dea6527df454f6c5a0fe375fa8edce
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2023-01-06 17:10:06 +0100

    intro title media query

--- src/components/Models/Model.js
+++ src/components/Models/Model.js
@@ -1,16 +1,10 @@
 import { Canvas } from '@react-three/fiber'
 import { useGLTF, OrbitControls, Environment, ContactShadows } from "@react-three/drei";
-import { useState, useEffect } from 'react';
 import NikePegasus from './Shoes/NikePegasus';
 
 function Model({ ModelGltf }) {
     const { materials, nodes } = useGLTF(ModelGltf);
-    const [Width, setWidth] = useState(window.innerWidth);
-    useEffect(() => {
-        window.addEventListener('resize', function () {
-            setWidth(window.innerWidth);
-        })
-    })
+
     return (
         <div id='canvas-container'>
             <Canvas shadows camera={{ position: [3, 0, 0], fov: 50 }}>
@@ -20,7 +14,7 @@ function Model({ ModelGltf }) {
                 <Environment preset="city" />
                 <ContactShadows position={[0, -0.8, 0]} opacity={0.25} scale={10} blur={1.5} far={0.8} />
                 <directionalLight castShadow />
-                <NikePegasus materials={materials} nodes={nodes} Width={Width} />
+                <NikePegasus materials={materials} nodes={nodes} />
             </Canvas>
 
         </div >
--- src/components/Models/Shoes/NikePegasus.js
+++ src/components/Models/Shoes/NikePegasus.js
@@ -1,18 +1,20 @@
-import { useState, useRef } from "react";
-const NikePegasus = ({ materials, nodes, Width }) => {
-    const [Hover, setHover] = useState(false);
+import { useState, useRef, useEffect } from "react";
+const NikePegasus = ({ materials, nodes }) => {
+    const [ScWidth, setWidth] = useState(window.innerWidth);
     const meshEl = useRef();
-    meshEl.current = Width;
-
+    meshEl.current = ScWidth;
+    useEffect(() => {
+        window.addEventListener('resize', function () {
+            setWidth(window.innerWidth);
+        })
+    })
     return (
         <mesh
             ref={meshEl}
             geometry={nodes.defaultMaterial.geometry}
             material={materials.NikeShoe}
-            position={[1, meshEl.current < 800 ? -0.3 : -0.3, 0]}
-            onPointerOver={() => setHover(!Hover)}
-            onPointerOut={() => setHover(false)}
-            scale={(meshEl.current < 500) ? 0.4 : ((meshEl.current > 500 && meshEl.current < 800) ? 0.7 : 1)
+            position={[0, meshEl.current < 800 ? -0.3 : -0.3, 0]}
+            scale={(meshEl.current < 768) ? 0.6 : ((meshEl.current > 768 && meshEl.current < 1024) ? 0.7 : (1024 < meshEl.current && meshEl.current < 1200) ? 1 : 1.3) //ask for refactoring this line ! 
             }
         // map={materials.NikeShoe.normalMap}
         />
