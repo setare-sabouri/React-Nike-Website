aa2790a256798201eff680fb4e0eb8b78dcc0176
Kevin Johnson <harlemworld1985@gmail.com>
2023-01-08 22:08:49 +0100

    featuredslideshow

---
+++ src/components/Sections/Features/features-cards.js
@@ -0,0 +1,9 @@
+
+const Card = ({ imageSrc }) => {
+    return (
+        <div className="card-wrapper">
+            <img className="card-img" src={imageSrc}></img>
+        </div >
+    )
+}
+export default Card;
--- src/components/Sections/Features/features.js
+++ src/components/Sections/Features/features.js
@@ -1,10 +1,18 @@
+import "../../../styles/features.css";
+import React from 'react';
+import FeaturesCarousel from "./features-carousel";
+
+
+
 
 const Features = () => {
     return (
-        <section className="">
+        <section className="component features-component">
             <h1 className="header-font">Featured</h1>
             <p className="paragraph-font">These are the newest style of the season.</p>
+            <FeaturesCarousel />
         </section>
+        
     )
 }
 
